26e797dddb1cf281d0fd2d140bbc7d5bd76e3c31
Miku Taniguchi <51152912+mikutaniguchi@users.noreply.github.com>
2025-07-27 10:55:58 +0900

    モーダルの背景色変更

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -38,7 +38,8 @@ export default function Modal({
 
   return (
     <div 
-      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50"
+      className="fixed inset-0 flex items-center justify-center p-4 z-50"
+      style={{ backgroundColor: 'rgba(248, 248, 248, 0.7)' }}
       onClick={onClose}
     >
       <div 
